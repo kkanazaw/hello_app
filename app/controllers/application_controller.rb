6f22307dc9216cb261d4974805a5832fadd8aaea
malmrashede <keigo1410@yahoo.co.jp>
2016-02-02 17:41:18 +0900

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
