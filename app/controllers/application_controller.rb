2521857198b855daecf81189a83e3b328a82ffe8
malmrashede <keigo1410@yahoo.co.jp>
2016-02-02 17:38:59 +0900

    1.8.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
+# -*- coding: undecided -*-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hola, mundo!"
+    render text: "¡Hola, mundo!"
   end
 end
